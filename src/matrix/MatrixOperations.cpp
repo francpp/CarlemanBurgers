3b491da9fa95432317bf251059d258c83e26dd23
Francesco Pettenon <francesco.pettenon@epfl.ch>
2024-07-26 16:44:34 +0000

    feat: add base matrix

---
+++ src/matrix/MatrixOperations.cpp
@@ -0,0 +1,19 @@
+#include "MatrixOperations.hpp"
+
+namespace sim::matrix
+{
+
+MatrixOperations::MatrixOperations(
+  const params::SimulationParameters        &params,
+  const sim::discretization::Discretization &discretization)
+{
+  // Constructor implementation
+}
+
+void
+MatrixOperations::createMatrices()
+{
+  // Matrix creation implementation
+}
+
+} // namespace sim::matrix
