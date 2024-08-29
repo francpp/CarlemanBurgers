120b2c5bdffb8b48434bf64e3e16266a8cfc55aa
Francesco Pettenon <francesco.pettenon@mail.polimi.it>
2024-08-29 15:26:34 +0000

    refactor: move carleman matrices to sparse

--- src/matrix/CarlemanMatrix.cpp
+++ src/matrix/CarlemanMatrix.cpp
@@ -1,5 +1,6 @@
 #include "CarlemanMatrix.hpp"
 #include "kronp.hpp"
+#include <utils/MatrixFormat.hpp>
 #include <cmath>
 #include <iostream>
 #include <numeric>
@@ -22,14 +23,14 @@ namespace matrix
     return dNs;
   }
 
-  Eigen::MatrixXd
+  Eigen::SparseMatrix<double>
   assembleCarlemanMatrix(int N_max, int nx, int ode_deg,
                          const Eigen::MatrixXd &F0, const Eigen::MatrixXd &F1,
                          const Eigen::MatrixXd &F2)
   {
     std::vector<int> dNs = calculateBlockSizes(N_max, nx);
     int             total_size = dNs.back();
-    Eigen::MatrixXd A = Eigen::MatrixXd::Zero(total_size, total_size);
+    Eigen::SparseMatrix<double> A(total_size, total_size);
 
     // Construct Fs by concatenating the first row of F0 as a column vector, F1,
     // and F2
@@ -37,7 +38,10 @@ namespace matrix
     Eigen::MatrixXd firstColumn =
       F0.row(0).transpose(); // Convert the first row of F0 into a column vector
     Fs << firstColumn, F1, F2;
-    // TODO
+
+    Eigen::SparseMatrix<double> Fs_sparse = Fs.sparseView();
+    Eigen::SparseMatrix<double> I(nx, nx);
+    I.setIdentity();
     for(int i = 1; i <= N_max; ++i)
       {
         for (int j = 0; j <= std::min(ode_deg, N_max - i + 1); ++j)
@@ -53,31 +57,27 @@ namespace matrix
             int b1 = b0 + std::pow(nx, j + i - 1) - 1;
 
             // Initialize Aij using Eigen::MatrixXd
-            Eigen::MatrixXd Aij =
-              Eigen::MatrixXd::Zero(std::pow(nx, i), std::pow(nx, j + i - 1));
+            Eigen::SparseMatrix<double> Aij(std::pow(nx, i),
+                                            std::pow(nx, j + i - 1));
             // Extract the relevant block from Fs
             int f0 = 1 + (std::pow(nx, j) - nx) / (nx - 1) +
                      1; // Adjusted for zero-based indexing
             int f1 = f0 + std::pow(nx, j) -
                      1; // fj and fj+1 difference is the number of rows of Fj
-            Eigen::MatrixXd Fj = Fs.block(0, f0 - 1, Fs.rows(), f1 - f0 + 1);
+            Eigen::SparseMatrix<double> Fj =
+              Fs_sparse.block(0, f0 - 1, Fs.rows(), f1 - f0 + 1);
             for (int p = 1; p <= i; ++p)
             {
                 // Calculate Kronecker products using Eigen matrices
-                Eigen::MatrixXd Ia =
-                  kronp(Eigen::MatrixXd::Identity(nx, nx), p - 1);
-                Eigen::MatrixXd Ib =
-                  kronp(Eigen::MatrixXd::Identity(nx, nx), i - p);
-
-                Eigen::MatrixXd kron_product = kron(Ia, Fj);
-                Aij += kron(kron_product,
-                            Ib); // Updated based on the MATLAB code's structure
+                Eigen::SparseMatrix<double> Ia = kronp(I, p - 1);
+                Eigen::SparseMatrix<double> Ib = kronp(I, i - p);
+                Eigen::SparseMatrix<double> kron_product = kron(Ia, Fj);
+                Aij += kron(kron_product, Ib);
             }
 
             // Insert the Aij block into the appropriate place in the full
             // matrix A
-            A.block(a0, b0, Aij.rows(), Aij.cols()) =
-              Aij; // Direct block assignment
+            matrixUtils::assignSparseBlock(A, Aij, a0, b0);
         }
       }
 
--- src/matrix/CarlemanMatrix.hpp
+++ src/matrix/CarlemanMatrix.hpp
@@ -13,10 +13,11 @@ namespace matrix
   std::vector<int> calculateBlockSizes(int N_max, int nx);
 
   // Function to assemble the Carleman matrix using the given parameters
-  Eigen::MatrixXd assembleCarlemanMatrix(int N_max, int nx, int ode_deg,
-                                         const Eigen::MatrixXd &F0,
-                                         const Eigen::MatrixXd &F1,
-                                         const Eigen::MatrixXd &F2);
+  Eigen::SparseMatrix<double> assembleCarlemanMatrix(int N_max, int nx,
+                                                     int ode_deg,
+                                                     const Eigen::MatrixXd &F0,
+                                                     const Eigen::MatrixXd &F1,
+                                                     const Eigen::MatrixXd &F2);
 
 } // namespace matrix
 } // namespace sim
